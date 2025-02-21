0941cbc35f8d16fe4843b3dc98ce74aeec92c624
Kunal Sareen <kunal.sareen@anu.edu.au>
2025-02-21 00:30:31 +0000

    Use `ImmortalSpace` as non-moving space

--- src/plan/mutator_context.rs
+++ src/plan/mutator_context.rs
@@ -412,9 +412,12 @@ pub(crate) fn create_allocator_mapping(
         reserved.n_large_object += 1;
 
         // TODO: This should be freelist allocator once we use marksweep for nonmoving space.
+        // map[AllocationSemantics::NonMoving] =
+        //     AllocatorSelector::FreeList(reserved.n_free_list);
+        // reserved.n_free_list += 1;
         map[AllocationSemantics::NonMoving] =
-            AllocatorSelector::FreeList(reserved.n_free_list);
-        reserved.n_free_list += 1;
+            AllocatorSelector::BumpPointer(reserved.n_bump_pointer);
+        reserved.n_bump_pointer += 1;
 
         // TODO(kunals): This required if we want to support plans other than Immix-style ones for ART
         // map[AllocationSemantics::Zygote] =
@@ -484,11 +487,16 @@ pub(crate) fn create_space_mapping<VM: VMBinding>(
         ));
         reserved.n_large_object += 1;
         // TODO: This should be freelist allocator once we use marksweep for nonmoving space.
+        // vec.push((
+        //     AllocatorSelector::FreeList(reserved.n_free_list),
+        //     plan.common().get_nonmoving(),
+        // ));
+        // reserved.n_free_list += 1;
         vec.push((
-            AllocatorSelector::FreeList(reserved.n_free_list),
+            AllocatorSelector::BumpPointer(reserved.n_bump_pointer),
             plan.common().get_nonmoving(),
         ));
-        reserved.n_free_list += 1;
+        reserved.n_bump_pointer += 1;
         if plan.common().is_zygote() {
             vec.push((
                 AllocatorSelector::Immix(reserved.n_immix),
