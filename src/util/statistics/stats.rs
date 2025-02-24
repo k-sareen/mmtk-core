3f41ffd116641e1d24c1419d0a1db5a0cc4b4aaf
Kunal Sareen <kunal.sareen@anu.edu.au>
2025-02-24 03:56:39 +0000

    WIP: Commit debugging code

--- src/util/statistics/stats.rs
+++ src/util/statistics/stats.rs
@@ -203,7 +203,7 @@ impl Stats {
             output_string.push_str(format!("{}\t", value).as_str());
         }
         output_string.push_str("\n------------------------------ End MMTk Statistics -----------------------------\n");
-        warn!("{}", output_string);
+        print!("{}", output_string);
     }
 
     pub fn print_column_names(
